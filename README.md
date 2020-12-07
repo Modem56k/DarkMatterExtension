b68486e323b53932319858d0de5a53786f64ddce
Cameron McFarland <ojomdab@gmail.com>
2020-12-06 21:30:38 -0500

    Rewrite

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ This plugin depends on the Foundry VTT DND5E system module to work.
 ## Install
 Use this manifest:
 `https://gitlab.com/csmcfarland/dmpoc/raw/master/module.json`
+
+## Thank You
+Special thank to those of you who have pointed me in the right direction
+or given me insight be letting me see your code, or even just helped
+by asking good questions or listening to me.
+* Calego#0914
+* Spetzel#0103
+* DoctorWho426#8439
--- esmodules/main.js
+++ esmodules/main.js
@@ -1,119 +1,21 @@
 import { DND5E } from '/systems/dnd5e/module/config.js';
 import Actor5e from '/systems/dnd5e/module/actor/entity.js';
-import Item5e from '/systems/dnd5e/module/item/entity.js';
 
 DND5E.skills["dat"] = "Data";
 DND5E.skills["pil"] = "Piloting";
 DND5E.skills["tec"] = "Technology";
 
-//Add the skills to the character sheet as a skill if, and only if, the actors being created are npcs or characters
-//If the type is vehicle, it makes no changes.
-const prepBase = Actor5e.prototype.prepareBaseData;
-
-function extendActorBaseData() {
-  console.log("DMPOC | extendActorBaseData");
-  console.log(this);
-  const dmskills = ['Piloting Skill', 'Data Skill', 'Technology Skill'];
-  const itemPiloting = {
-    name: 'Piloting Skill',
-    type: 'feat',
-    data: {
-      ability: 'dex',
-      formula: 'd20 + @mod + @prof',
-      actionType: 'util',
-      description: {
-        value: '<p>You pilot!</p>',
-        chat: 'woah!',
-        unidentified: 'what?'
-      }
-    }
-  };
-  const pilFound = this.data.items.some(el => el.name === 'Piloting Skill');
-  if(pilFound) {
-    console.log("DMPOC |  Piloting Skill Exists");
-  } else {
-    console.log("DMPOC |  Adding Piloting Skill");
-    this.createOwnedItem(itemPiloting);
-  }
-  const itemTechnology = {
-    name: 'Technology Skill',
-    type: 'feat',
-    data: {
-      ability: 'int',
-      formula: 'd20 + @mod + @prof',
-      actionType: 'util',
-      description: {
-        value: '<p>You technology!</p>',
-        chat: 'woah!',
-        unidentified: 'what?'
-      }
-    }
-  };
-  const tecFound = this.data.items.some(el => el.name === 'Technology Skill');
-  if(tecFound) {
-    console.log("DMPOC |  Technology Skill Exists");
-  } else {
-    console.log("DMPOC |  Adding Technology Skill");
-    this.createOwnedItem(itemTechnology);
-  }
-  const itemData = {
-    name: 'Data Skill',
-    type: 'feat',
-    data: {
-      ability: 'int',
-      formula: 'd20 + @mod + @prof',
-      actionType: 'util',
-      description: {
-        value: '<p>You data!</p>',
-        chat: 'woah!',
-        unidentified: 'what?'
-      }
-    }
-  };
-  const datFound = this.data.items.some(el => el.name === 'Data Skill');
-  if(datFound) {
-    console.log("DMPOC |  Data Skill Exists");
-  } else {
-    console.log("DMPOC |  Adding Data Skill");
-    this.createOwnedItem(itemData);
-  }
-
-  // if (this.data.type === "npc" || this.data.type === "character") {} else {
-  //   const atr = this.data.data.attributes;
-  //   atr["maneuver"] = atr["maneuver"] || 0;
-  //   atr["engineclass"] = atr["engineclass"] || 0;
-  // }
-  return prepBase.call(this);
-}
-Actor5e.prototype.prepareBaseData = extendActorBaseData;
-
-// An attempt to shim patch into the derived data method
-const prepDerived = Actor5e.prototype.prepareDerivedData;
-
-function extendActorDerivedData() {
-  console.log("DMPOC | extendActorDerivedData");
-  let skills = this.data.data.skills;
-  const pilFound = typeof(skills.pil);
-  console.log("TEEEST");
-  console.log(pilFound);
-  if(pilFound === 'undefined'){
-    skills.pil = {
-      "value": 0,
-      "ability": "dex"
-    }
-  }
-  // for (let [id, skl] of Object.entries(this.data.data.skills)) {
-  //   console.log(id);
-  //   console.log(skl);
-  // }
-  return prepDerived.call(this);
-}
-//Actor5e.prototype.prepareDerivedData = extendActorDerivedData;
-
-// console.log("DMPOC | File Loaded");
-
-Hooks.on("init", function() {
+Hooks.on("init", async function() {
   console.log("DMPOC | This code runs once the Foundry VTT software begins it's initialization workflow.");
+  const prepData = Actor5e.prototype.prepareData;
+  function extendActor5ePrepareData() {
+    const skills = this._data.data.skills;
+    skills['pil'] = { value: 0, ability: 'dex', ...skills['pil'] };
+    skills['dat'] = { value: 0, ability: 'int', ...skills['dat'] };
+    skills['tec'] = { value: 0, ability: 'int', ...skills['tec'] };
+    return prepData.call(this);
+  }
+  Actor5e.prototype.prepareData = extendActor5ePrepareData;
 });
 
 Hooks.on("ready", function() {
